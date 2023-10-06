3ad0a2716c6c6d43a60a5e5a5757bc8a3bd24d7d
Matheus <matheuznardi@gmail.com>
2023-10-06 10:58:18 -0300

    POO

---
+++ Payments/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Payments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class Pagamento
+    {
+        //Propriedade
+        DateTime Vencimento;
+        //Métodos-Funções
+        void Pagar() { }
+    }
+}
